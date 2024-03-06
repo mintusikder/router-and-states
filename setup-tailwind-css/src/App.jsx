4c139502faba4dddfa0fe0aeb234daece463f1a6
mintusikder <mintusikder15@gmail.com>
2024-03-06 14:24:44 +0600

    first commit

---
+++ setup-tailwind-css/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import NavBar from "./assets/components/NavBar/NavBar";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <h1 className="text-xl font-bold underline bg-red-500">Hello world!</h1>
+      <NavBar></NavBar>
+    </>
+  );
+}
+
+export default App;
