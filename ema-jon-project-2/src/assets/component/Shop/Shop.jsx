fde0a52ec105b18d88a0330fb236445df94377aa
mintusikder <mintusikder15@gmail.com>
2024-03-12 10:23:35 +0600

    save cart to local storage

--- ema-jon-project-2/src/assets/component/Shop/Shop.jsx
+++ ema-jon-project-2/src/assets/component/Shop/Shop.jsx
@@ -4,6 +4,7 @@ import './Shop.css'
 import { useEffect } from 'react';
 import Product from '../Product/Product';
 import Cart from '../Cart/Cart';
+import { addToDb, getShoppingCart } from '../../utilities/fakedb';
 const Shop = () => {
     const [cart, setCart] = useState([])
     const [products, setProducts] = useState([])
@@ -12,9 +13,16 @@ const Shop = () => {
         .then(res => res.json())
         .then(data => setProducts(data))
     },[])
+    useEffect(()=>{
+       const getCart = getShoppingCart()
+       console.log(getCart)
+    },[])
+
+    
     const handelAddToCart = (product) =>{
         const newCart = [...cart, product]
         setCart(newCart)
+        addToDb(product.id)
       }
     return (
         <div className='shop-container'>
